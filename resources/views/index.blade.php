2f67b5b1ec5dca5df791449fa98b425661d984ed
snidima <snidima@mail.ru>
2017-09-04 21:37:02 +0300

    index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,4 +5,15 @@
     @include('index/section-1')
     @include('index/section-2')
     @include('index/section-3')
+
+
+    <div id="menu-content" style="display: none;">
+        <div class="menu-wrapper">
+            <div class="menu">
+                <a href="#section-3" class="menu__item">Главная</a>
+                <a href="#" class="menu__item">Для кого этот курс</a>
+                <a href="#" class="menu__item">Цены</a>
+            </div>
+        </div>
+    </div>
 @endsection
