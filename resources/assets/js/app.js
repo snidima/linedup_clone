4781205de405b795fc7ae42e4b4477ab1b6aee93
snidima <snidima@mail.ru>
2017-09-27 03:01:49 +0300

    error test

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,7 +6,7 @@ import Login from './components/Login.vue';
 import Vue from 'vue';
 
 
-Vue.component( 'register', Register );
+Vue.component( 'registerr', Register );
 Vue.component( 'login', Login );
 
 if( $('#app').length ){
