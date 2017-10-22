c37108cba6e9477eefdb37d58c07a1dabb839483
snidima <snidima@mail.ru>
2017-10-22 19:51:45 +0300

    change courses logic

--- resources/views/index/section-4.blade.php
+++ resources/views/index/section-4.blade.php
@@ -1,39 +1,106 @@
 <section class="section-4 section_price section" id="section-3">
-    <div class="section_price-bg"></div>
     <div class="container">
         <h3 class="h3">Цены на видеокурс</h3>
 
-        <div class="price">
-            <div class="price__item price-item">
-                <div class="price-item__wrapper">
-                    <div class="price-item__title">Базовый пакет</div>
-                    <div class="price-item__items-wrapper">
-                        <div class="price-item__items-item">10 видео уроков</div>
-                        <div class="price-item__items-item">200 часов интеснивного обучения</div>
-                        <div class="price-item__items-item">Сертификат</div>
+
+        <div class="course-thumbs">
+
+            <div class="course-thumbs__item course-thumb">
+                <div class="course-thumb__block course-thumb-block">
+                    <div class="course-thumb-block__price">
+                        <span>2 000 руб.</span><br>
+                        <span>1750 руб.</span>
                     </div>
-                    <div class="price-item__price">6 500 <small>руб.</small></div>
+                    <div class="course-thumb-block__header course-thumb-block-header">
+                        <div class="course-thumb-block-header__title">Adobe After Effect. Стандартный</div>
+                        <div class="course-thumb-block-header__info course-thumb-block-header-info">
+                            <div class="course-thumb-block-header-info__item">18 октября 2017 г. - 23 октября 2017 г.</div>
+                            <div class="course-thumb-block-header-info__item">Продолжительность: 64 дня</div>
+                            <div class="course-thumb-block-header-info__item">10 уроков</div>
+                        </div>
+                    </div>
+                    <div class="course-thumb-block__content course-thumb-block-content">
+                        <div class="course-thumb-block-content__title">
+                            Описание курса
+                        </div>
+                        <div class="course-thumb-block-content__text">
+                            Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+                            Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.
+                            В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов,
+                            используя Lorem Ipsum для распечатки образцов.
+                        </div>
+                    </div>
                 </div>
-                <div class="price-item__buy">
-                    <div class="btn btn-big btn-type-1 btn-type-1_black">Купить</div>
+                <div class="course-thumb__btn">
+                    <div class="btn btn-big btn-type-1"><i class="fa fa-cart-plus" aria-hidden="true"></i>Купить курс</div>
                 </div>
             </div>
-            <div class="price__item price-item price-item_2">
-                <div class="price-item__wrapper">
-                    <div class="price-item__title">Базовый пакет</div>
-                    <div class="price-item__items-wrapper">
-                        <div class="price-item__items-item">10 видео уроков</div>
-                        <div class="price-item__items-item">200 часов интеснивного обучения</div>
-                        <div class="price-item__items-item">Сертификат</div>
-                        <div class="price-item__items-item">20 часов констультаций</div>
+
+            <div class="course-thumbs__item course-thumb">
+                <div class="course-thumb__block course-thumb-block">
+                    <div class="course-thumb-block__price">
+                        4 000 руб.
                     </div>
-                    <div class="price-item__price">6 500 <small>руб.</small></div>
+                    <div class="course-thumb-block__header course-thumb-block-header">
+                        <div class="course-thumb-block-header__title">Adobe After Effect. Расширенный</div>
+                        <div class="course-thumb-block-header__info course-thumb-block-header-info">
+                            <div class="course-thumb-block-header-info__item">18 октября 2017 г. - 23 октября 2017 г.</div>
+                            <div class="course-thumb-block-header-info__item">Продолжительность: 64 дня</div>
+                            <div class="course-thumb-block-header-info__item">10 уроков</div>
+                        </div>
+                    </div>
+                    <div class="course-thumb-block__content course-thumb-block-content">
+                        <div class="course-thumb-block-content__title">
+                            Описание курса
+                        </div>
+                        <div class="course-thumb-block-content__text">
+                            Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+                            Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.
+                            В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов,
+                            используя Lorem Ipsum для распечатки образцов.
+                        </div>
+                    </div>
                 </div>
-                <div class="price-item__buy">
-                    <div class="btn btn-big btn-type-1 btn-type-1_black">Купить</div>
+                <div class="course-thumb__btn">
+                    <div class="btn btn-big btn-type-1"><i class="fa fa-cart-plus" aria-hidden="true"></i>Купить курс</div>
                 </div>
             </div>
+
+
+
         </div>
+
+        {{--<div class="price">--}}
+            {{--<div class="price__item price-item">--}}
+                {{--<div class="price-item__wrapper">--}}
+                    {{--<div class="price-item__title">Базовый пакет</div>--}}
+                    {{--<div class="price-item__items-wrapper">--}}
+                        {{--<div class="price-item__items-item">10 видео уроков</div>--}}
+                        {{--<div class="price-item__items-item">200 часов интеснивного обучения</div>--}}
+                        {{--<div class="price-item__items-item">Сертификат</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="price-item__price">6 500 <small>руб.</small></div>--}}
+                {{--</div>--}}
+                {{--<div class="price-item__buy">--}}
+                    {{--<div class="btn btn-big btn-type-1 btn-type-1_black">Купить</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="price__item price-item price-item_2">--}}
+                {{--<div class="price-item__wrapper">--}}
+                    {{--<div class="price-item__title">Базовый пакет</div>--}}
+                    {{--<div class="price-item__items-wrapper">--}}
+                        {{--<div class="price-item__items-item">10 видео уроков</div>--}}
+                        {{--<div class="price-item__items-item">200 часов интеснивного обучения</div>--}}
+                        {{--<div class="price-item__items-item">Сертификат</div>--}}
+                        {{--<div class="price-item__items-item">20 часов констультаций</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="price-item__price">6 500 <small>руб.</small></div>--}}
+                {{--</div>--}}
+                {{--<div class="price-item__buy">--}}
+                    {{--<div class="btn btn-big btn-type-1 btn-type-1_black">Купить</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
     </div>
 </section>
