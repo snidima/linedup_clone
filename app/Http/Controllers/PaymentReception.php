99e478e609f97dc3d2486f2344220fcb6e674ed4
snidima <snidima@mail.ru>
2017-10-29 22:01:31 +0300

    payment..

--- app/Http/Controllers/PaymentReception.php
+++ app/Http/Controllers/PaymentReception.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 
 
+use App\Models\Billing;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -14,16 +15,24 @@ class PaymentReception extends Controller
     public function yandex( Request $request )
     {
 
-        Log::info('YANDEX REQUEST',[
-            'headers' => $request->header(),
-            'data' => $request->all(),
-        ] );
+        $billing = Billing::find( json_decode( $request->input('label') )->orderId );
 
-        $payOK = false;
-        if( $payOK )
-            return response(null, 200);
-        else
-            return response(null, 406);
+        $billing->amount = $request->input('withdraw_amount');
+        $billing->information = json_encode( $request->all() );
+
+        $billing->save();
+
+
+
+        return response(null, 200);
+
+//
+//
+//        $payOK = false;
+//        if( $payOK )
+//            return response(null, 200);
+//        else
+//            return response(null, 406);
     }
 
 
