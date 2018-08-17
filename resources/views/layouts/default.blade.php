c4b4d2742c679205a3054cd254c2d670a09707d2
snidima <snidima@mail.ru>
2018-08-17 11:56:06 +0700

    prepare to release

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -32,9 +32,13 @@
 @if( \Illuminate\Support\Facades\App::environment() == 'production' )
 <!-- BEGIN JIVOSITE CODE {literal} -->
 <script type='text/javascript' async>
-    (function(){ var widget_id = 'KLJez83FBH';var d=document;var w=window;function l(){
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
+(function(){ var widget_id = 'KLJez83FBH';var d=document;var w=window;function l(){
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
 <!-- {/literal} END JIVOSITE CODE -->
+
+<!-- VK PIXEL -->
+<script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?159",t.onload=function(){VK.Retargeting.Init("VK-RTRG-273862-33vu2"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-273862-33vu2" style="position:fixed; left:-999px;" alt=""/></noscript>
+<!-- VK PIXEL -->
 @endif
 </body>
 </html>
