2d18fc90d289da0141ceeef45f1ae1b20b5aa467
snidima <snidima@mail.ru>
2018-08-20 02:25:01 +0700

    response

--- resources/views/buy-course.blade.php
+++ resources/views/buy-course.blade.php
@@ -48,8 +48,3 @@
         </section>
     </div>
 @endsection
-@push('afterScripts')
-    <!--VK PIXEL-->
-    <script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?159",t.onload=function(){VK.Retargeting.Init("VK-RTRG-274592-6tQZp"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-274592-6tQZp" style="position:fixed; left:-999px;" alt=""/></noscript>
-    <!--/VK PIXEL-->
-@endpush
