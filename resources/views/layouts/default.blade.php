e45f69a9e7f0a1819d34b73fd2711c5e4c945d1b
snidima <snidima@mail.ru>
2018-08-20 01:14:40 +0700

    response

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -37,10 +37,10 @@
 var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
 <!-- END JIVOSITE CODE -->
 <!-- VK PIXEL -->
-<script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?159",t.onload=function(){VK.Retargeting.Init("VK-RTRG-273862-33vu2"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-273862-33vu2" style="position:fixed; left:-999px;" alt=""/></noscript>
+<script type="text/javascript" async>!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?159",t.onload=function(){VK.Retargeting.Init("VK-RTRG-273862-33vu2"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-273862-33vu2" style="position:fixed; left:-999px;" alt=""/></noscript>
 <!-- /VK PIXEL -->
 <!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
+<script type="text/javascript" async>
     (function (d, w, c) {
         (w[c] = w[c] || []).push(function() {
             try {
@@ -74,7 +74,6 @@ var s = document.createElement('script'); s.type = 'text/javascript'; s.async = 
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-
     gtag('config', 'UA-68464550-3');
 </script>
 @endif
