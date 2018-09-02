6eef53896c173b1490b27bd6c1748eb652df3e41
snidima <snidima@mail.ru>
2018-09-02 14:38:29 +0700

    price block change

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -31,6 +31,23 @@
 @show
 
 @if( \Illuminate\Support\Facades\App::environment() == 'production' )
+<!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2105312883130791');
+    fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=2105312883130791&ev=PageView&noscript=1"
+    /></noscript>
+<!-- End Facebook Pixel Code -->
 <!-- BEGIN JIVOSITE CODE -->
 <script type='text/javascript' async>
 (function(){ var widget_id = 'KLJez83FBH';var d=document;var w=window;function l(){
