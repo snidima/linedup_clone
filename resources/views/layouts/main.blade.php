57bf47f93dc3d5618767ff973d8ce8ef70b54ffd
snidima <snidima@mail.ru>
2018-09-07 12:36:15 +0700

    price block change

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,6 +2,15 @@
 @section('title','Школа видеомонтажа - личный кабинет')
 
 @section('html')
+    @if( \Illuminate\Support\Facades\Auth::check() )
+        <div class="container" style="padding: 10px 0;">
+        @if( array_intersect(\Illuminate\Support\Facades\Auth::user()->roles->pluck('slug')->toArray(), ['administrator']) )
+                <a href="/administrator#/lessons">Настройки курса</a> |
+                <a href="/manager/clients">Клиенты</a> |
+                <a href="/teacher/homeworks">Домашние задания</a>
+        @endif
+        </div>
+    @endif
     @include('partials/header')
     @yield('content')
 @endsection
