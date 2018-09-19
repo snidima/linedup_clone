b5e0cb61a1263e4191886f79620c1f05e4aebdb9
snidima <snidima@mail.ru>
2018-09-20 00:13:44 +0700

    check

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::defaultView('pagination::bootstrap-4');
+        Paginator::defaultView('pagination::bootstrap-4'); 
     }
 
     /**
